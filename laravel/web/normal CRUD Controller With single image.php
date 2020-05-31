6397b35c9966856fa2f68f50b98ee5049093b9e2
Sabuz <smskushtia@gmail.com>
2020-05-31 11:27:18 +0600

    Crud controller modified

---
+++ laravel/web/normal CRUD Controller With single image.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use App\Category;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::orderBy('id','desc')->paginate(10);
+        return view('post.index',compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        return view('post.create',compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        //dd($request->all());
+        try{
+            $message = [];
+            $rules = [];
+
+            $validator = Validator::make($request->all(), $rules, $message);
+            if ($validator->fails()) {
+                return redirect()->back()->withInput()->withErrors($validator);
+            }
+
+
+            
+            if ($request->has('photo')) {
+                $imageName = time().'.'.$request->photo->extension();  
+   
+                $request->photo->move(public_path('images'), $imageName);
+                // Set user profile image path in database to filePath
+                
+            }
+
+            Post::create([
+                'category_id' => $request->category_id,
+                'title' => $request->title,
+                'description' => $request->description,
+                'photo' => $imageName,
+            ]);
+            alert()->success('Post Created');
+
+            return redirect()->route('posts.index');
+
+        }catch(\Exception $e){
+            return $e->getMessage();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $categories = Category::all();
+        return view('post.edit',compact('post','categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        try{
+            $message = [
+                'title.required' => 'Post title  is required',
+                
+                'description.required' => 'Post Description  is required',
+                'photo.required' => 'Please put an image',
+                'photo.mimes' => 'This is not an image',
+                
+                
+                
+            ];
+            $rules = [
+                'title' => 'required',
+                'description' => 'required',
+                'photo' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
+                
+               
+            ];
+            $validator = Validator::make($request->all(), $rules, $message);
+            if ($validator->fails()) {
+                return redirect()->back()->withInput()->withErrors($validator);
+            }
+
+            if ($request->has('photo')) {
+                $imageName = time().'.'.$request->photo->extension();  
+   
+                $request->photo->move(public_path('images'), $imageName);
+                // Set user profile image path in database to filePath
+                
+            }
+
+           
+
+            $post->title = $request->title;
+            $post->category_id = $request->category_id;
+            $post->description = $request->description;
+            $post->photo = $imageName;
+            $post->save();
+            alert()->success('Post updated');
+
+            return redirect()->route('posts.index');
+
+        }catch(\Exception $e){
+            return $e->getMessage();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        unlink(public_path() . '/images/'. $post->photo);
+        $post->delete();
+        alert()->success('Post Deleted');
+
+        return redirect()->route('posts.index');
+    }
+}
