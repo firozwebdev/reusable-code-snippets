f30f8e8ab20c030270ae722989881ae07716dd59
Sabuz <smskushtia@gmail.com>
2020-05-31 13:18:59 +0600

    seeding file modified

--- laravel/web/Seeding/factory-seeding.php
+++ laravel/web/Seeding/factory-seeding.php
@@ -3,7 +3,9 @@
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        'name' => $faker->firstName(),
+        'name' => $faker->name,
+        'first_name' => $faker->firstName(),
+        'last_name' => $faker->lastName(),
         'category_id' => App\Category::all()->random()->id,
         'title' => $faker->lastName(),
         'price' =>  $faker->numberBetween(10,100),
@@ -12,3 +14,4 @@ $factory->define(App\Product::class, function (Faker $faker) {
         
     ];
 });
+
