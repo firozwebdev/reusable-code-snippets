0b67894773b6457ddfcb958fc9db32da08010559
Sabuz <smskushtia@gmail.com>
2020-06-01 13:09:57 +0600

    navigation file modified

--- laravel/web/navigation-or-menubar-active.php
+++ laravel/web/navigation-or-menubar-active.php
@@ -6,8 +6,10 @@ $route = Route::current()->getName();
 
 //sidebar link will be like below for active or menu-open class
 
-{{ ($prefix == '/products') ? 'menu-open':''}} // generally for parent menu
-{{ ($route == 'products.create') ? 'active':''}} // generally for child menu
+{{ ($prefix == '/products') ? 'menu-open':''}} // checking by prefix generally for parent menu
+{{ ($route == 'products.create') ? 'active':''}} // checking by route generally for child menu
+
+{{ request()->is('/dashboard') ? ' active' : '' }} // checking by url
 
 
 
