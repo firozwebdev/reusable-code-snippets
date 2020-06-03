501d17bc0a6511f9cbf6d4badacbd8414cbe8c49
Sabuz <smskushtia@gmail.com>
2020-06-03 06:49:54 +0600

    blade utility function added

---
+++ laravel/web/blade-utility-function.php
@@ -0,0 +1,21 @@
+<?php
+
+
+@php
+
+@endphp
+
+
+
+/*
+    comma will not be seen for last item in side a loop
+
+*/
+
+@foreach($categories as $category)
+    {{ $category->name }}
+    @if(! $loop->last) 
+        ,
+    @endif
+@endforeach
+
