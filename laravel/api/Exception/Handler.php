b50a8711e19ab59ac55064c1438906d09f375a50
Sabuz <smskushtia@gmail.com>
2020-06-04 13:42:48 +0600

    exception handler added

---
+++ laravel/api/Exception/Handler.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+    If we want to handle exception, then we  have to write code inside  render method just like below
+
+*/
+
+
+
+
+
+namespace App\Exceptions;
+
+use Exception;
+use App\Traits\ApiResponser;
+
+use Illuminate\Database\QueryException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
+class Handler extends ExceptionHandler
+{
+    use ApiResponser;
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+        parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $exception)
+    {
+        if($exception instanceof ValidationException){
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+        if($exception instanceof ModelNotFoundException){
+            $modelName = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("Does not exists any $modelName with the specified identificator",404);
+        }
+
+        if($exception instanceof AuthenticationException) {
+            return $this->errorResponse('Unauthenticated !', 401);
+        }
+        if($exception instanceof AuthorizationException) {
+            return $this->errorResponse($exception->getMessage(), 403);
+        }
+        
+        if($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse('The specified url can not be found', 404);
+        }
+
+        if($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse('The specified method for the request can not be found', 405);
+        }
+        if($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+        if($exception instanceof QueryException) {
+            $errorCode = $exception->errorInfo[1];
+            if($errorCode == 1451){
+                return $this->errorResponse('Can not remove this resource permanently. It is related with any other resource',409);
+            }
+           
+        }
+
+        //return $this->errorResponse('Unexpected Exception. Try later', 500);
+
+
+        return parent::render($request, $exception);
+    }
+
+
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request){
+        $errors = $e->validator->errors()->getMessages();
+
+        return $this->errorResponse($errors,422);
+    }
+}
