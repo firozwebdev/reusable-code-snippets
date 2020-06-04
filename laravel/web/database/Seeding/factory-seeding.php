e33777d7baeb3583b9504e38eb29808bb0fecb67
Sabuz <smskushtia@gmail.com>
2020-06-04 11:47:58 +0600

    factory file modified

--- laravel/web/database/Seeding/factory-seeding.php
+++ laravel/web/database/Seeding/factory-seeding.php
@@ -15,3 +15,32 @@ $factory->define(App\Product::class, function (Faker $faker) {
     ];
 });
 
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1,10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT,Product::UNAVAILABLE_PRODUCT]), // this constant defined in Product model
+        'image' => $faker->randomElement(['1.jpg','2.jpg','3.jpg']),
+        'seller_id' => User::all()->random()->id,
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker $faker) {
+
+    $seller = Seller::has('products')->get()->random();
+    $buyer = User::all()->except($seller->id)->random();
+    return [
+        
+        'quantity' => $faker->numberBetween(1,3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
+    ];
+});
