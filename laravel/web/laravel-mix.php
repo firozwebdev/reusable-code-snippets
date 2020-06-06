12811f42d01a62b44e79c680fdbe927a90c2cd22
Sabuz <smskushtia@gmail.com>
2020-06-06 17:14:32 +0600

    my css file added to laravel mix

---
+++ laravel/web/laravel-mix.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * যদি লারাভেল মিকস ফাইলে নেজের css file এ্ড করতে চাই
+ *
+ *
+ *
+ *
+ * 1. first create a directory named css inside  resources folder
+ * 2. Then create .css file (style.css) inside that directory (css/style.css)
+ * 3. then add  @import "../css/style.css"; at last line in app.scss inside resources/sass directory.
+ * 4. Then run npm run watch
+ *
+ */
+
