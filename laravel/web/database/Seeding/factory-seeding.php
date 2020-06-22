3e2b05736b15e2ad2dfe3c8af2d4e3d09a6d28c3
Sabuz <smskushtia@gmail.com>
2020-06-22 17:36:50 +0600

    factory file modified

--- laravel/web/database/Seeding/factory-seeding.php
+++ laravel/web/database/Seeding/factory-seeding.php
@@ -44,3 +44,33 @@ $factory->define(Transaction::class, function (Faker $faker) {
         'product_id' => $seller->products->random()->id,
     ];
 });
+
+
+$factory->define(\App\Models\Product::class, function (Faker $faker) {
+    $image1 = $faker->randomElement(['/demo/1.jpg','/demo/2.jpg','/demo/7.jpg']);
+    $image2 = $faker->randomElement(['/demo/2.jpg','/demo/7.jpg','/demo/3.jpg']);
+    $image3 = $faker->randomElement(['/demo/3.jpg','/demo/6.jpg','/demo/1.jpg']);
+    $image4 = $faker->randomElement(['/demo/4.jpg','/demo/3.jpg','/demo/2.jpg']);
+    $image5 = $faker->randomElement(['/demo/5.jpg','/demo/1.jpg','/demo/3.jpg']);
+    $image6 = $faker->randomElement(['/demo/6.jpg','/demo/3.jpg','/demo/4.jpg']);
+    return [
+        'product_name' => $faker->name,
+        'product_title' => $faker->text,
+        'category_id' => \App\Models\Category::all()->random()->id,
+        'type' => $faker->randomElement(['Regular','Featured','Special']),
+        'slug' => Str::slug($faker->firstName),
+        'product_price' => $faker->numberBetween(100,1000),
+        'special_price' => $faker->numberBetween(100,1000)-50,
+        'start_date' => \Carbon\Carbon::now(),
+        'end_date' => \Carbon\Carbon::now(),
+        'product_quantity' => $faker->numberBetween(5,50),
+        'sku' => Str::random(4),
+        'stock' => $faker->numberBetween(5,50),
+        'multiple' => json_encode([$image1,$image2,$image3,$image4,$image5,$image6]),
+        'description' => 'চীনের সঙ্গে লাদাখে সং দুই দেশের মধ্যে বিরোধপূর্ণ সীমান্ত এলাকায় গোলাগুলি ও বিস্ফোরকের ব্যবহার নিষিদ্ধ করা হয় ওই চুক্তিতে।',
+        'color' => json_encode(['yellow','Black']),
+        'video_link' => 'https://www.youtube.com/watch?v=E2wa0BTHRMs',
+        'warranty' => $faker->numberBetween(1,3),
+        'status' => '1',
+    ];
+});
